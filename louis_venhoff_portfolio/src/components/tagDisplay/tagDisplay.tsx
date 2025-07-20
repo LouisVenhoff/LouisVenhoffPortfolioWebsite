9fbe3f820cbdcef5a2b18cdeddf34005b9a8dc27
Louis Venhoff <Louis.venhoff@gmx.net>
2025-07-20 15:25:08 +0200

    Set restart always policy

---
+++ louis_venhoff_portfolio/src/components/tagDisplay/tagDisplay.tsx
@@ -0,0 +1,102 @@
+import React, { JSX, Ref, useEffect, useRef, useState} from "react";
+import * as motion from "motion/react-client";
+import { Badge } from "@chakra-ui/react";
+import Doc from "../../classes/doc";
+import "../../styles/components/tagDisplayStyle.css";
+
+type TagDisplayProps = {
+    currentDoc: Doc;
+}
+
+
+const TagDisplay:React.FC<TagDisplayProps> = ({currentDoc}) => {
+
+    
+    const [collapsed, setCollapsed] = useState<boolean>(true);
+    
+    const tagsDiv:Ref<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        
+        if(!tagsDiv.current) return;
+        
+        tagsDiv.current.addEventListener("mouseenter", () => {
+            decollapse();
+        });
+
+        tagsDiv.current.addEventListener("mouseleave", () => {
+            collapse();
+        });
+    }, []);
+
+    
+    const renderTagsCollapsedState = ():JSX.Element[] => {
+        if(!currentDoc) return [];
+
+        let badges:JSX.Element[] = [];
+
+        const visibleTagCount = currentDoc.tags.length < 5 ? currentDoc.tags.length : 5;
+        
+        for(let i = 0; i < visibleTagCount; i++){
+            badges.push(<Badge key={i} maxW="sm" backgroundColor="#000000" color="teal">{currentDoc.tags[i]}</Badge>);
+        }
+
+        let hiddenTagCount: number = currentDoc.tags.length - visibleTagCount;
+
+        if(hiddenTagCount > 0){
+            badges.push(<p className="tag-display--hidden-tag-count">{`+${hiddenTagCount}`}</p>);
+        }
+
+        return badges;
+
+    }
+
+    const collapse = () => {
+        setCollapsed(true);
+    }
+
+    const decollapse = () => {
+        setCollapsed(false);
+    }
+
+    const renderAllTags = ():JSX.Element => {
+        if(!currentDoc) return <></>;
+
+        let badges:JSX.Element[] = [];
+        let secondRowBadges:JSX.Element[] = [];
+
+        const alwaysVisibleTagCount = currentDoc.tags.length < 5 ? currentDoc.tags.length : 5;
+
+        for(let i = 0; i < currentDoc.tags.length; i++){
+            
+            if(i <= alwaysVisibleTagCount){
+                badges.push(<Badge key={i} maxW="sm" backgroundColor="#000000" color="teal">{currentDoc.tags[i]}</Badge>);
+            }
+            else{
+                secondRowBadges.push(<Badge key={i} maxW="sm" backgroundColor="#000000" color="teal">{currentDoc.tags[i]}</Badge>);
+            }
+        }
+        
+        return(<motion.div animate={{ height: 50, animationDuration: 100 }} className="tag-display--full-container">
+                    <div className="tag-display"> 
+                        {badges}
+                    </div>
+                    <div className="tag-display">
+                        {secondRowBadges}
+                    </div>
+                </motion.div>);
+    }
+
+    return(
+        <div className="flex flex-row">
+            <div ref={tagsDiv}>
+                <div className="tag-display">
+                    {collapsed ? renderTagsCollapsedState() : renderAllTags()}
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default TagDisplay;
