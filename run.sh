ec539eca90c07d43844b0dbb160ce730edd49abd
Louis Venhoff <Louis.venhoff@gmx.net>
2025-12-30 11:54:15 +0100

    Enhanced deploy scripts

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 echo "Setting up Production environment for LouisVenhoff Portfolio"
 
-echo "ℹ Updating sources"
-
-git pull
-
 if docker info 2>/dev/null | grep -q "Swarm: active"; then
     echo "✔ Swarm found"
 else
     echo "⚠ Please initialize a swarm on this device!"
+    echo "ℹ Use this command: sudo docker swarm init"
     exit 0
 fi
 
@@ -17,11 +14,13 @@ IMAGE_NAME="portfolio_app"
 if docker stack ls --format '{{.Name}}' | grep -qx "$STACK_NAME"; then
   echo "ℹ Deleting old stack"
 
-  docker stack rm portfolio
+  docker stack rm $STACK_NAME
 fi
 
 if docker image ls | grep -q "$IMAGE_NAME"; then
     echo "ℹ Deleting outdated image"
+
+    docker rmi $IMAGE_NAME
 fi
 
 echo "ℹ Building new image"
