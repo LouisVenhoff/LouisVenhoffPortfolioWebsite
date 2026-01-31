d24b2d99431eaa1e6455d514bc9af4f9aff79bbc
Louis Venhoff <Louis.venhoff@gmx.net>
2026-01-31 01:29:32 +0100

    Created graphQL DTO

--- portfolio_backend/Dto/graphql/ContributionData.cs
+++ portfolio_backend/Dto/graphql/ContributionData.cs
@@ -1,32 +1,33 @@
 namespace portfolio_backend.Dto;
 
-public class ContributionDay
-{
-    public string date { get; set; }
-    public int contributionCount { get; set; }
-}
-
-public class Week
-{
-    public List<ContributionDay> contributionDays { get; set; }
-}
-
-public class ContributionCalendar
-{
-    public List<Week> weeks { get; set; }
-}
-
-public class ContributionsCollection
-{
-    public ContributionCalendar contributionCalendar { get; set; }
-}
-
-public class User
-{
-    public ContributionsCollection contributionsCollection { get; set; }
-}
-
 public class UserResponse
 {
     public User user { get; set; }
 }
+
+public class User
+{
+    public ContributionsCollection contributionsCollection;
+}
+
+public class ContributionsCollection
+{
+    public ContributionsCalendar contributionsCalendar;
+}
+
+public class ContributionsCalendar
+{
+    public List<Week> weeks;
+}
+
+public class Week
+{
+    public List<ContributionDay> contributionsDays;
+}
+
+public class ContributionDay
+{
+    public DateTime date;
+    public int contributionCount;
+}
+
