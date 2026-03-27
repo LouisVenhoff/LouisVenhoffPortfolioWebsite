60622d000035bc317d6fdb07224cdd08b26fd897
Louis Venhoff <Louis.venhoff@gmx.net>
2026-03-27 16:11:11 +0100

    Created file for useContribution Hook

---
+++ louis_venhoff_portfolio/src/hooks/useContribution.ts
@@ -0,0 +1,20 @@
+export type Contribution = {
+    time: Date;
+    count: number;
+}
+
+
+export default function useContribution(){
+
+    const loadContributionList = ():Contribution[] => {
+        
+        
+        
+        return [];
+    }
+
+
+    return { loadContributionList }
+
+
+}
