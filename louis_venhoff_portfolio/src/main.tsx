c136fdf1013261e985c88a9ad5ef787428b864eb
Louis Venhoff <Louis.venhoff@gmx.net>
2026-04-08 23:03:25 +0200

    Removed all graphql related stuff

--- louis_venhoff_portfolio/src/main.tsx
+++ louis_venhoff_portfolio/src/main.tsx
@@ -2,17 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
-import { ApolloProvider } from '@apollo/client';
-import useGithubApi from './hooks/useGithubApi.ts';
-
-const graphQLClient = useGithubApi();
 
 
 createRoot(document.getElementById('root')!).render(    
     <ChakraProvider value={defaultSystem}>
-        <ApolloProvider client={graphQLClient}>
-            <App />
-        </ApolloProvider>
+        <App />
     </ChakraProvider>
 );
 
